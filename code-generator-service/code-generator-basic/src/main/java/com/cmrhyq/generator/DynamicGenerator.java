1976f005b43cbe55bff022af2b0b9780619a5e06
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-03-29 00:24:59 +0800

    1.FreeMarker测试代码，生成java代码，方法封装

--- code-generator-service/code-generator-basic/src/main/java/com/cmrhyq/generator/DynamicGenerator.java
+++ code-generator-service/code-generator-basic/src/main/java/com/cmrhyq/generator/DynamicGenerator.java
@@ -25,24 +25,44 @@ import java.io.Writer;
 public class DynamicGenerator {
 
     public static void main(String[] args) throws IOException, TemplateException {
-        Configuration configuration = new Configuration(Configuration.VERSION_2_3_32);
-
-        configuration.setDirectoryForTemplateLoading(new File("src/main/resources/templates"));
-
-        configuration.setNumberFormat("0.####");
-
-        configuration.setDefaultEncoding("utf-8");
-
-        Template template = configuration.getTemplate("Template.java.ftl");
-
+        String projectPath = System.getProperty("user.dir");
+        String inputPath = projectPath + File.separator + "src/main/resources/templates/Template.java.ftl";
+        String outputPath = projectPath + File.separator + "Template.java";
         TemplateConfig templateConfig = new TemplateConfig();
         templateConfig.setLoop(true);
-        templateConfig.setAuthor("Alan");
-        templateConfig.setOutputText("Sum:");
+        templateConfig.setAuthor("Alan Huang 1");
+        templateConfig.setOutputText("Sum1 = ");
+        doGenerator(inputPath, outputPath, templateConfig);
+    }
 
-        Writer out = new FileWriter("Template.java");
+    /**
+     * 文件生成
+     * @param inputPath 输入路径
+     * @param outputPath 输出路径
+     * @param model {"loop":true,"author":"Alan Huang","outputText":"Sum:"}
+     * @throws IOException
+     * @throws TemplateException
+     */
+    public static void doGenerator(String inputPath, String outputPath, Object model) throws IOException, TemplateException {
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_32);
+        // 模板文件所在路径
+        File templateDir = new File(inputPath).getParentFile();
+        configuration.setDirectoryForTemplateLoading(templateDir);
+        // 设置字符集，数字格式
+        configuration.setNumberFormat("0.####");
+        configuration.setDefaultEncoding("utf-8");
+        // 加载指定模板
+        String templateName = new File(inputPath).getName();
+        Template template = configuration.getTemplate(templateName);
 
-        template.process(templateConfig, out);
+        // 创建数据模型
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setLoop(false);
+        templateConfig.setAuthor("Alan Huang");
+        templateConfig.setOutputText("Sum = ");
+
+        Writer out = new FileWriter(outputPath);
+        template.process(model, out);
 
         out.close();
     }
